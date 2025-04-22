b6f9795ee41d5d1ed9c7c2eabbeddc3d014cb679
lb <justinmaks79@gmail.com>
2025-04-21 23:04:59 -0400

    rm-lg

--- app/components/LegalDisclaimer.tsx
+++
@@ -1,8 +0,0 @@
-export const LegalDisclaimer = () => {
-  return (
-    <div className="text-sm text-gray-500 mt-4">
-      <p>This application is for educational purposes only. Content is provided by third-party services. 
-         Please ensure you have the right to access any content you stream.</p>
-    </div>
-  );
-}; 
